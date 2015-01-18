b3a8705e8e4266e59125b6234ff1aa96a029cf9c
David Liang <liangchenye@gmail.com>
2015-01-18 14:29:18 +0800

    generate monthly report

---
+++ GithubUser/git_data_analysis/monthly_amount/README.md
@@ -0,0 +1,18 @@
+# Ospaf GithubUser Documentation
+
+## based on github openAPI, basic authentication
+
+# find monthly amount
+ - Command: 'python monthly_amount.py'
+ - Explain: get the total amount of each month
+
+# generate monthly report
+ - Command: 'python monthly_report.py'
+ - Explain: based on monthly_amount.py
+
+# txt file
+ - Command: 'python monthly_report.py > my_report.txt'
+ - note: 'id' is not related too closely to 'created_at time'
+
+- - -
+Copyright 2014 Ospaf Lab Software, Inc. Unless otherwise marked, this work is licensed under a [Creative Commons Attribution 3.0 Unported License](http://creativecommons.org/licenses/by/3.0/).
---
+++ GithubUser/git_data_analysis/monthly_amount/monthly_report.py
@@ -0,0 +1,56 @@
+import base64
+import json
+import re
+import sys
+import datetime
+import pymongo
+from pymongo import MongoClient
+
+# The first user is mojombo, id == 1, created: 2007-10-20
+
+# The very late one is githublover001 id == 10293416 updated: 2015-01-06
+          
+
+def init_db ():
+    _db_addr = "127.0.0.1"
+    _db_port = 27017
+    _db_name = "github"
+
+    client = MongoClient(_db_addr, _db_port)
+    return client[_db_name]
+
+# month_str is something like 2010-04
+def report_by_month (db, month_str):
+    saved_res = db["research_result"].find_one({"type": "monthly_amount", "month": month_str})
+    if saved_res:
+        gap = 0
+        if saved_res.has_key('first') and saved_res.has_key('last'):
+            gap = saved_res["last"]["id"] - saved_res["first"]["id"]
+        print month_str + "  <total_public " + str(saved_res["total_public"]) + '> <id gap ' + str(gap) + '>'
+    else:
+        return
+
+#begin at 2007-10, end at 2015-01
+def report_months(db):
+    begin_date = {"year": 2007, "month": 10}
+    end_date = {"year": 2015, "month": 1}
+    for year in range(begin_date["year"], end_date["year"]+1):
+        begin_month = 1
+        end_month = 12
+        if year == begin_date["year"]:
+            begin_month = begin_date["month"]
+        elif year == end_date["year"]:
+            end_month = end_date["month"]
+
+        for month in range(begin_month, end_month+1):
+            month_str = "%d-%02d"%(year, month)
+            report_by_month(db, month_str)
+
+def main ():
+    db = init_db()
+    if (db):
+        report_months(db)
+    else:
+        print "Cannot connect to database"
+
+main()
